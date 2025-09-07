140ef9b8851bd09a12d1e91e4a97e122ab4812d7
bnithin06 <bnithin2822@gmail.com>
2025-09-07 10:37:11 +0530

    code updated

--- network-status/index.js
+++ network-status/index.js
@@ -2,13 +2,13 @@ console.log('Network status script loaded');
 const statusElement = document.getElementById('status');
 console.log(statusElement);
 
-// window.addEventListener('online', () => {
-//     statusElement.innerHTML='🌍 You are online';
-// });
+window.addEventListener('online', () => {
+    statusElement.innerHTML='🌍 You are online';
+});
 
-// window.addEventListener('offline', () => {
-//     statusElement.innerHTML='🌐 You are offline';
-// });
+window.addEventListener('offline', () => {
+    statusElement.innerHTML='🌐 You are offline';
+});
 
 
-navigator.onLine ? statusElement.innerHTML='🌍 You are online' : statusElement.innerHTML='🌐 You are offline';
+// navigator.onLine ? statusElement.innerHTML='🌍 You are online' : statusElement.innerHTML='🌐 You are offline';
